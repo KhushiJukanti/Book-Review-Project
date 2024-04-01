3b3df43d5f9c57aa1c2ceae7b20eab5b3905cf29
Khushi <khushijukanti33@gmail.com>
2024-04-01 17:51:25 +0530

    add review project frontend backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Updated import statement
+import BookListingPage from './components/BookListingPage';
 
-import './App.css';
 import Navbar from './components/Navbar';
+import ParentComponent from './components/ParentComponent';
 
 function App() {
   return (
+
+
     <div className="App">
       <Navbar />
+      <BookListingPage />
+      {/* <Routes> Wrap your Route components inside a Routes component */}
+      {/* <Route exact path="/" element={<BookListingPage />} />
+          <Route path="/books/:id" element={<BookDetailsPage />} />
+        </Routes> */}
     </div>
+
   );
 }
 
